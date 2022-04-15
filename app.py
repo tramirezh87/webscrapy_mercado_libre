1530a4f2adbdaccf64e6ae85ab9aaaade480865a
tramirezh87 <tramirezh87@gmail.com>
2022-04-15 03:13:41 +0000

    m

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from bs4 import BeautifulSoup
-from datetime import datetime 
+from datetime import datetime
+import requests 
+
 # pip install beautifulsoup4
 
 url="https://listado.mercadolibre.com.pe/camionetas#D[A:camionetas]"
 
+
 print("[INFO] {0}: Inicio del proceso".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+page = requests.get(url)
+soup = BeautifulSoup(page.content,'html.parser')
 
+lista_autos=[] 
+for x in soup.find_all(class_='ui-search-item__title ui-search-item__group__element'):
+    anuncio={}
+    anuncio['titulo']=x.get_text()
+    lista_autos.append(anuncio)
 
+print(lista_autos)
 
 print("[INFO] {0}: Fin del proceso".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
