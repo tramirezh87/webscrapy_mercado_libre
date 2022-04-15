8029a44008d35ef599fb0028ca7b1ab515e9b746
tramirezh87 <tramirezh87@gmail.com>
2022-04-15 03:02:35 +0000

    g

---
+++ app.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from datetime import datetime 
+# pip install beautifulsoup4
+
+url="https://listado.mercadolibre.com.pe/camionetas#D[A:camionetas]"
+
+print("[INFO] {0}: Inicio del proceso".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+
+
+
+print("[INFO] {0}: Fin del proceso".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
